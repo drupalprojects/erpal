7b3f741367e549551a707307cadb0df75b497758
soysavanh <soysavanh@brightsolutions.de>
2012-02-15 13:32:33 +0100

    fixed caching issues. jar file versioning.

--- www/sites/all/modules/custom/fileedit/fileedit_xml.tpl.php
+++ www/sites/all/modules/custom/fileedit/fileedit_xml.tpl.php
@@ -2,18 +2,20 @@
     <information>
         <title>File Handler</title>
         <vendor>Bright Solutions GmbH</vendor>
+		<homepage href="http://www.brightsolutions.de" />
+		<description>Utility Application to Down- and Upload Attachments from/into Drupal Nodes</description> 
     </information>
     <resources>
         <j2se version="1.6+" href="http://java.sun.com/products/autodl/j2se"/>
-        <jar href="<?php  echo $args['filehandler_jar_url']; ?>" main="true" />
-        <jar href="<?php  echo $args['commons_logging_jar_url']; ?>" download="eager" />
-        <jar href="<?php  echo $args['httpclient_jar_url']; ?>" download="eager" />
-        <jar href="<?php  echo $args['httpcore_jar_url']; ?>" download="eager" />
-		<jar href="<?php  echo $args['httpmime_jar_url']; ?>" download="eager" />
+        <jar href="<?php  echo $args['filehandler_jar_url']; ?>" main="true" version="0.0.1" />
+        <jar href="<?php  echo $args['commons_logging_jar_url']; ?>" download="eager" version="1.1.1" />
+        <jar href="<?php  echo $args['httpclient_jar_url']; ?>" download="eager" version="4.1.2" />
+        <jar href="<?php  echo $args['httpcore_jar_url']; ?>" download="eager" version="4.1.2" />
+		<jar href="<?php  echo $args['httpmime_jar_url']; ?>" download="eager" version="4.1.2" />
     </resources>
-<security>
-<all-permissions/>
-</security>    
+	<security>
+		<all-permissions/>
+	</security>    
     <application-desc
          name="File Handler"
          main-class="de.brightsolutions.filehandler.FileHandler">
