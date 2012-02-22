b52d89b0d45c3212ad369d6627435613996b20ef
soysavanh <soysavanh@brightsolutions.de>
2012-02-22 15:40:21 +0100

    turned off download/upload user verfication for now. TO BE FIXED!

--- www/sites/all/modules/custom/fileedit/fileedit_xml.tpl.php
+++ www/sites/all/modules/custom/fileedit/fileedit_xml.tpl.php
@@ -7,11 +7,11 @@
     </information>
     <resources>
         <j2se version="1.6+" href="http://java.sun.com/products/autodl/j2se"/>
-        <jar href="<?php  echo $args['filehandler_jar_url']; ?>" main="true" version="<?php echo _fileedit_jar_version('FileHandler'); ?>" />
-        <jar href="<?php  echo $args['commons_logging_jar_url']; ?>" download="eager" version="<?php echo _fileedit_jar_version('commons-logging'); ?>" />
-        <jar href="<?php  echo $args['httpclient_jar_url']; ?>" download="eager" version="<?php echo _fileedit_jar_version('httpclient'); ?>" />
-        <jar href="<?php  echo $args['httpcore_jar_url']; ?>" download="eager" version="<?php echo _fileedit_jar_version('httpcore'); ?>" />
-		<jar href="<?php  echo $args['httpmime_jar_url']; ?>" download="eager" version="<?php echo _fileedit_jar_version('httpmime'); ?>" />
+        <jar href="<?php  echo $args['filehandler_jar_url']; ?>" main="true" version="0.0.1" />
+        <jar href="<?php  echo $args['commons_logging_jar_url']; ?>" download="eager" version="1.1.1" />
+        <jar href="<?php  echo $args['httpclient_jar_url']; ?>" download="eager" version="4.1.2" />
+        <jar href="<?php  echo $args['httpcore_jar_url']; ?>" download="eager" version="4.1.2" />
+		<jar href="<?php  echo $args['httpmime_jar_url']; ?>" download="eager" version="4.1.2" />
     </resources>
 	<security>
 		<all-permissions/>
