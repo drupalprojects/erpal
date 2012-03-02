89c030df1e909fae3c4e5ed4223dbbaf539fd85b
Sebastian Kauschke <kauschke@brightsolutions.de>
2012-03-02 13:29:13 +0100

    Initial Commit for the bs_syncml module

---
+++ www/sites/all/modules/custom/bs_syncml/classes/SyncProcessor.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of SyncProcessor
+ *
+ * @author kauschke
+ */
+class SyncProcessor {
+  //put your code here
+}
+
+?>
