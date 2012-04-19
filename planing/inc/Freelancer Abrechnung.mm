2a5cc8952fac888589790d24cc550294066b60fc
Manuel Pistner <pistner@brightsolutions.de>
2012-04-19 23:04:53 +0200

    Freelancer Abrechnung.mm angepasst

--- planing/inc/Freelancer Abrechnung.mm
+++ planing/inc/Freelancer Abrechnung.mm
@@ -13,15 +13,14 @@
       - Wenn ein Task auf completed gesetzt wird und
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="14"/>
 <node CREATED="1334758890639" ID="ID_934603784" MODIFIED="1334758893102" POSITION="right" TEXT="profile">
 <node CREATED="1334758853902" ID="ID_1881439016" MODIFIED="1334758916301" TEXT="contact_ref">
 <node CREATED="1334760938190" ID="ID_500408048" MODIFIED="1334760945750" TEXT="refernziert einen erpal_contact"/>
 <node CREATED="1334760945953" ID="ID_1875921895" MODIFIED="1334760953721" TEXT="nur mit bestimmtem Recht &#xe4;nderbar"/>
 </node>
-<node CREATED="1334758917973" ID="ID_1564469349" MODIFIED="1334758970186" TEXT="field_collection contract_data">
+<node CREATED="1334758917973" FOLDED="true" ID="ID_1564469349" MODIFIED="1334869139348" TEXT="multiple field_collection contract_data">
 <node CREATED="1334758970187" ID="ID_1725902349" MODIFIED="1334758978868" TEXT="g&#xfc;ltig ab datum"/>
 <node CREATED="1334758979427" ID="ID_42598740" MODIFIED="1334759072917" TEXT="typ">
 <node CREATED="1334758993789" ID="ID_1792567017" MODIFIED="1334758996435" TEXT="freelancer"/>
@@ -36,16 +35,16 @@
 <node CREATED="1334759525979" ID="ID_1450977096" MODIFIED="1334759530357" TEXT="currency"/>
 <node CREATED="1334759530558" ID="ID_773071658" MODIFIED="1334759552870" TEXT="zahlungsziel (credit period)"/>
 </node>
-<node CREATED="1334759091736" FOLDED="true" ID="ID_666379478" MODIFIED="1334759169065" TEXT="Anwesenheit pro Woche regelm&#xe4;&#xdf;ig / plan">
+<node CREATED="1334759091736" FOLDED="true" ID="ID_666379478" MODIFIED="1334869068037" TEXT="Anwesenheit pro Woche regelm&#xe4;&#xdf;ig / plan">
 <node CREATED="1334759101725" ID="ID_1742649343" MODIFIED="1334759158392" TEXT="checkbox tage + eingabe Stunden (ohne genaue Uhrzeit)"/>
 </node>
-<node CREATED="1334759175913" FOLDED="true" ID="ID_582890564" MODIFIED="1334759633055" TEXT="kosten netto">
+<node CREATED="1334759175913" FOLDED="true" ID="ID_582890564" MODIFIED="1334869093538" TEXT="kosten netto">
 <node CREATED="1334759204693" ID="ID_1197783475" MODIFIED="1334759230637" TEXT="inkl. lohnnebenkosten"/>
 <node CREATED="1334759230863" ID="ID_1249283329" MODIFIED="1334759248909" TEXT="inkl soz abgaben bei festen ma"/>
 <node CREATED="1334759253839" ID="ID_694251459" MODIFIED="1334759259113" TEXT="ohne vat bei freelancern"/>
 </node>
-<node CREATED="1334759633922" FOLDED="true" ID="ID_1919033062" MODIFIED="1334760959875" TEXT="G&#xfc;ltig f&#xfc;r Project Tag">
-<node CREATED="1334759651945" ID="ID_1966134731" MODIFIED="1334759666068" TEXT="es kann nur eine Setting im gleichen Zeitraum f&#xfc;r gleiches Tag geben"/>
+<node CREATED="1334759633922" FOLDED="true" ID="ID_1919033062" MODIFIED="1334869129419" TEXT="G&#xfc;ltig f&#xfc;r Project Tag">
+<node CREATED="1334759651945" ID="ID_1966134731" MODIFIED="1334869128213" TEXT="es kann nur eine Setting im gleichen Zeitraum f&#xfc;r gleiches Tag geben"/>
 </node>
 <node CREATED="1334760960672" ID="ID_1424126357" MODIFIED="1334760975792" TEXT="nur mit bestimmtem recht ansehbar"/>
 <node CREATED="1334760976000" ID="ID_1642316729" MODIFIED="1334760981737" TEXT="nur mit bestimmtem recht &#xe4;nderbar"/>
@@ -72,7 +71,7 @@
 </node>
 </node>
 <node CREATED="1334758873570" ID="ID_1309184907" MODIFIED="1334758879881" POSITION="right" TEXT="billable">
-<node CREATED="1334759776411" ID="ID_1808358446" MODIFIED="1334760716497" TEXT="Erstellt wenn: siehe body">
+<node CREATED="1334759776411" ID="ID_1808358446" MODIFIED="1334869244601" TEXT="Erstellt wenn: siehe body">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -84,11 +83,14 @@
     <p>
       Wenn ein Task mit vollst&#228;ndigen EK Pricing informationen auf Completed gesetzt wird. der Kunde ist dann die eigene Firma, der contractor der contact der zu den Pricing informationen hinterlegt ist.
     </p>
+    <p>
+      Beim Speichern der EK Informationen muss validiert werden, dass diese Vollst&#228;ndig sind.
+    </p>
   </body>
 </html>
 </richcontent>
 </node>
-<node CREATED="1334760824258" ID="ID_420839048" MODIFIED="1334760870533" TEXT="behandeln wie f&#xfc;r forderungen">
+<node CREATED="1334760824258" ID="ID_420839048" MODIFIED="1334869248338" TEXT="behandeln wie f&#xfc;r forderungen">
 <node CREATED="1334760833242" ID="ID_1647696882" MODIFIED="1334760842746" TEXT="l&#xf6;schen, anpassen etc."/>
 </node>
 <node CREATED="1334760848073" ID="ID_1217035326" MODIFIED="1334761151459" TEXT="neues Feld contractor">
@@ -99,14 +101,18 @@
 <node CREATED="1334758880113" ID="ID_1639483521" MODIFIED="1334758886083" POSITION="right" TEXT="invoice">
 <node CREATED="1334761126275" ID="ID_192387088" MODIFIED="1334761151459" TEXT="field contractor">
 <arrowlink DESTINATION="ID_1217035326" ENDARROW="Default" ENDINCLINATION="53;0;" ID="Arrow_ID_517463090" STARTARROW="None" STARTINCLINATION="53;0;"/>
+<node CREATED="1334869316856" ID="ID_1619581643" MODIFIED="1334869326177" TEXT="wenn leer, eigene Firma einf&#xfc;gen"/>
 </node>
-<node CREATED="1334761157982" ID="ID_57765662" MODIFIED="1334761192089" TEXT="gescannte Rechnung hintelegen">
+<node CREATED="1334761157982" FOLDED="true" ID="ID_57765662" MODIFIED="1334869293386" TEXT="gescannte Rechnung hintelegen">
 <node CREATED="1334761192090" ID="ID_589448888" MODIFIED="1334761207448" TEXT="pdf generieren nicht m&#xf6;glich, wenn contractor != eigene Firma"/>
 <node CREATED="1334761215939" ID="ID_1895080158" MODIFIED="1334761227541" TEXT="wenn gel&#xf6;scht, billables unbiled setzen wie bei Forderungen"/>
 </node>
+<node CREATED="1334869294197" ID="ID_1635968424" MODIFIED="1334869301212" TEXT="Validierung">
+<node CREATED="1334869301213" ID="ID_767185977" MODIFIED="1334869311130" TEXT="zus&#xe4;tzlich muss contractor gleich sein"/>
+</node>
 </node>
 <node CREATED="1334759265682" ID="ID_898064015" MODIFIED="1334760812360" POSITION="right" TEXT="F&#xe4;lle">
-<node CREATED="1334759269037" FOLDED="true" ID="ID_1284384552" MODIFIED="1334759699518" TEXT="Reisekosten">
+<node CREATED="1334759269037" ID="ID_1284384552" MODIFIED="1334869347518" TEXT="Reisekosten">
 <node CREATED="1334759273102" ID="ID_840010210" MODIFIED="1334759286626" TEXT="task mit ek festpreis, completed"/>
 </node>
 <node CREATED="1334759700410" ID="ID_414499945" MODIFIED="1334759709803" TEXT="Subunternehmer festpreis">
@@ -116,6 +122,15 @@
 <node CREATED="1334760813158" ID="ID_817473202" MODIFIED="1334760817233" POSITION="right" TEXT="Modulaufbau">
 <node CREATED="1334761264974" ID="ID_824259290" MODIFIED="1334761281528" TEXT="packen wir mit in ERPAL_projects"/>
 <node CREATED="1334761286182" ID="ID_801199975" MODIFIED="1334761295856" TEXT="erpal_invoice anpassen"/>
+<node CREATED="1334869367852" ID="ID_802233240" MODIFIED="1334869413981" TEXT="erpal_profiles">
+<node CREATED="1334869413982" ID="ID_625713541" MODIFIED="1334869432346" TEXT="ben&#xf6;tigt">
+<node CREATED="1334869432348" ID="ID_1493842194" MODIFIED="1334869434496" TEXT="erpal_basic"/>
+</node>
+<node CREATED="1334869425974" ID="ID_568999453" MODIFIED="1334869440229" TEXT="ben&#xf6;tigt von">
+<node CREATED="1334869440231" ID="ID_990534256" MODIFIED="1334869445656" TEXT="erpal_projects"/>
+<node CREATED="1334869445871" ID="ID_867507004" MODIFIED="1334869450057" TEXT="mitarbeiterverwaltung"/>
+</node>
+</node>
 </node>
 </node>
 </map>
