f0580afe15f47a281600c2c5d12957e14c32857e
Manuel Pistner <pistner@brightsolutions.de>
2012-05-25 00:22:51 +0200

    Keep on working with document management system

--- www/sites/all/modules/custom/erpal_book_helper/template/book_pdf_html.tpl.php
+++ www/sites/all/modules/custom/erpal_book_helper/template/book_pdf_html.tpl.php
@@ -1,6 +1,8 @@
-<p>
 <?php 
-$params = $pdf_object->serializeTCPDFtagParameters($node->title,1,0,'','',array(128,128,0));  //needed later for table of contentes 
+$params = $pdf_object->serializeTCPDFtagParameters(array($node->title,1,0,'','',array(128,128,0)));  //needed later for table of contentes 
+
 echo '<tcpdf method="Bookmark" params="'.$params.'" />';
-echo $node->title; ?>
-</p>
+//$pdf_object->Bookmark($node->title, 0, 0, '', 'B', array(0,64,128));
+echo "<h1>".$node->title."</h1>"; 
+echo $node->body[LANGUAGE_NONE][0]['value'];  //this should be handeled with view modes
+?>
--- www/sites/all/modules/custom/erpal_print/template/toc.tpl.php
+++ www/sites/all/modules/custom/erpal_print/template/toc.tpl.php
@@ -1,6 +1,7 @@
 <?php
   $pdf_object->addTOCPage();
   $pdf_object->setXY(10,30,'');
-  $pdf_object->addTOC(2, 'courier', '.', t('Table of contents'), 'I', array(128,0,0));
+  $pdf_object->setTitle($title);
+  $pdf_object->addTOC(1, 'courier', '.', $title, 'I', array(128,0,0));
   $pdf_object->endTOCPage();
 ?>
