ff68a5e8426ce3aa0c990f859b159f5e2ea9244f
Manuel Pistner <pistner@brightsolutions.de>
2012-10-15 11:04:26 +0200

    added erpal context links block to tickets pane and recreated features due to activation of date repeat module

---
+++ www/sites/all/modules/custom/erpal_basic_helper/template/context_links.tpl.php
@@ -0,0 +1,9 @@
+<div class="erpal_context_links">
+<?php
+//theme context links in ERPAL
+  foreach ($links as $link) {
+    print "<div class='erpal_context_link'>".$link."</div>";
+  }
+?>
+
+</div>
