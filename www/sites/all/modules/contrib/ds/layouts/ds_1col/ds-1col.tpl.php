16952c357f0b4a52eed0beb50052db9d792d45ee
Manuel Pistner <pistner@brightsolutions.de>
2012-10-17 01:42:51 +0200

    Switched back to display suite branch 1 because of incompatibilities

--- www/sites/all/modules/contrib/ds/layouts/ds_1col/ds-1col.tpl.php
+++ www/sites/all/modules/contrib/ds/layouts/ds_1col/ds-1col.tpl.php
@@ -5,15 +5,11 @@
  * Display Suite 1 column template.
  */
 ?>
-<<?php print $ds_content_wrapper; print $layout_attributes; ?> class="ds-1col <?php print $classes;?> clearfix">
+<div class="ds-1col <?php print $classes;?> clearfix <?php print $ds_content_classes;?>">
 
   <?php if (isset($title_suffix['contextual_links'])): ?>
   <?php print render($title_suffix['contextual_links']); ?>
   <?php endif; ?>
 
   <?php print $ds_content; ?>
-</<?php print $ds_content_wrapper ?>>
-
-<?php if (!empty($drupal_render_children)): ?>
-  <?php print $drupal_render_children ?>
-<?php endif; ?>
+</div>
--- www/sites/all/modules/contrib/ds/layouts/ds_2col_stacked_fluid/ds-2col-stacked-fluid.tpl.php
+++ www/sites/all/modules/contrib/ds/layouts/ds_2col_stacked_fluid/ds-2col-stacked-fluid.tpl.php
@@ -4,40 +4,40 @@
  * @file
  * Display Suite fluid 2 column stacked template.
  */
-
+?>
+<?php
   // Add sidebar classes so that we can apply the correct width in css.
   if (($left && !$right) || ($right && !$left)) {
     $classes .= ' group-one-column';
   }
 ?>
-<<?php print $layout_wrapper; print $layout_attributes; ?> class="ds-2col-stacked-fluid <?php print $classes;?> clearfix">
+<div class="ds-2col-stacked-fluid <?php print $classes;?> clearfix">
 
   <?php if (isset($title_suffix['contextual_links'])): ?>
   <?php print render($title_suffix['contextual_links']); ?>
   <?php endif; ?>
 
-  <<?php print $header_wrapper ?> class="group-header<?php print $header_classes; ?>">
-    <?php print $header; ?>
-  </<?php print $header_wrapper ?>>
+  <?php if ($header): ?>
+    <div class="group-header<?php print $header_classes; ?>">
+      <?php print $header; ?>
+    </div>
+  <?php endif; ?>
 
   <?php if ($left): ?>
-    <<?php print $left_wrapper ?> class="group-left<?php print $left_classes; ?>">
+    <div class="group-left<?php print $left_classes; ?>">
       <?php print $left; ?>
-    </<?php print $left_wrapper ?>>
+    </div>
   <?php endif; ?>
 
   <?php if ($right): ?>
-    <<?php print $right_wrapper ?> class="group-right<?php print $right_classes; ?>">
+    <div class="group-right<?php print $right_classes; ?>">
       <?php print $right; ?>
-    </<?php print $right_wrapper ?>>
+    </div>
   <?php endif; ?>
 
-  <<?php print $footer_wrapper ?> class="group-footer<?php print $footer_classes; ?>">
-    <?php print $footer; ?>
-  </<?php print $footer_wrapper ?>>
-
-</<?php print $layout_wrapper ?>>
-
-<?php if (!empty($drupal_render_children)): ?>
-  <?php print $drupal_render_children ?>
-<?php endif; ?>
+  <?php if ($footer): ?>
+    <div class="group-footer<?php print $footer_classes; ?>">
+      <?php print $footer; ?>
+    </div>
+  <?php endif; ?>
+</div>
