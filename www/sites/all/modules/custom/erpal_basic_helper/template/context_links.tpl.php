2c11368b4a81ce9000cca87d4bcce1be5511fc9a
Manuel Pistner <pistner@brightsolutions.de>
2012-10-19 14:29:27 +0200

    created new menu structure with local actions for projects, tasks and tickets

--- www/sites/all/modules/custom/erpal_basic_helper/template/context_links.tpl.php
+++
@@ -1,9 +0,0 @@
-<div class="erpal_context_links">
-<?php
-//theme context links in ERPAL
-  foreach ($links as $link) {
-    print "<div class='erpal_context_link'>".$link."</div>";
-  }
-?>
-
-</div>
