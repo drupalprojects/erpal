637869ffecc801614219bbc6329b6a9320fae201
Manuel Pistner <pistner@brightsolutions.de>
2012-10-22 17:56:03 +0200

    added own 2xx layout for panels

---
+++ www/sites/all/themes/erpal/plugins/layouts/erpal_content_layout/erpal-content-layout-template.tpl.php
@@ -0,0 +1,35 @@
+<div<?php print $css_id ? " id=\"$css_id\"" : ''; ?> class="<?php print $classes; ?>">
+
+ <div id="columns">
+    <div class="columns-inner clearfix">
+      <div id="content-column" role="main">
+        <div class="content-inner">
+            <?php if (!empty($content['sidebar_first_left'])): ?>
+				<div class="region region-sidebar-left">
+				  <?php print render($content['sidebar_first_left']); ?>
+				</div>
+          <?php endif; ?>
+		  <?php if (!empty($content['sidebar_first_right'])): ?>
+				<div class="region region-sidebar-right">
+				  <?php print render($content['sidebar_first_right']); ?>
+				</div>
+          <?php endif; ?>
+        </div>
+		<div class="content-inner" class="content-item">
+            <?php if (!empty($content['sidebar_second_left'])): ?>
+				<div class="region region-sidebar-left">
+				  <?php print render($content['sidebar_second_left']); ?>
+				</div>
+          <?php endif; ?>
+		  <?php if (!empty($content['sidebar_second_right'])): ?>
+				<div class="region region-sidebar-right">
+				  <?php print render($content['sidebar_second_right']); ?>
+				</div>
+          <?php endif; ?>
+        </div>
+
+
+      </div>
+  </div>
+</div>
+</div>
