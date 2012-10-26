ae7f630b4224de8a2ecc35d07664587682e96152
Manuel Pistner <pistner@brightsolutions.de>
2012-10-26 06:48:54 +0200

    fixed row style bug

--- www/sites/all/themes/erpal/plugins/layouts/erpal_task_view_row_layout/erpal-task-view-row-layout.tpl.php
+++ www/sites/all/themes/erpal/plugins/layouts/erpal_task_view_row_layout/erpal-task-view-row-layout.tpl.php
@@ -28,14 +28,14 @@
           <?php endif; ?>
         </div>
 		<div class="content-inner">
-            <?php if (!empty($content['sidebar_second_left'])): ?>
+            <?php if (!empty($content['sidebar_third_left'])): ?>
 				<div class="region region-sidebar-left">
-				  <?php print render($content['sidebar_second_left']); ?>
+				  <?php print render($content['sidebar_third_left']); ?>
 				</div>
           <?php endif; ?>
-		  <?php if (!empty($content['sidebar_second_right'])): ?>
+		  <?php if (!empty($content['sidebar_third_right'])): ?>
 				<div class="region region-sidebar-right">
-				  <?php print render($content['sidebar_second_right']); ?>
+				  <?php print render($content['sidebar_third_right']); ?>
 				</div>
           <?php endif; ?>
         </div>
