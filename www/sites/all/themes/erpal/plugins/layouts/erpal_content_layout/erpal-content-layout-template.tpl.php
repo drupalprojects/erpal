801b42ffff4d5cb62ef252ce34d63bcd94ee6ee0
U-BENUTZER\sony <sony@julia-pc.(none)>
2012-11-22 20:02:14 +0200

    updated id to class for layout

--- www/sites/all/themes/erpal/plugins/layouts/erpal_content_layout/erpal-content-layout-template.tpl.php
+++ www/sites/all/themes/erpal/plugins/layouts/erpal_content_layout/erpal-content-layout-template.tpl.php
@@ -1,8 +1,8 @@
 <div<?php print $css_id ? " id=\"$css_id\"" : ''; ?> class="<?php print $classes; ?>">
 
- <div id="columns">
+ <div class="columns">
     <div class="columns-inner clearfix">
-      <div id="content-column" role="main">
+      <div class="content-column" role="main">
         <div class="content-inner">
             <?php if (!empty($content['sidebar_first_left'])): ?>
 				<div class="region region-sidebar-left<?php if (empty($content['sidebar_first_right'])): ?> single-row<?php endif; ?>">
