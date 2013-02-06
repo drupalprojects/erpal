f72ffe562feb55e2965256c59f2e7c04b5fe5b81
xeagle <xeagle>
2013-02-06 22:01:50 +0200

    Improve fileedit module to support recently change FileHandler to download, open, monitor, upload files. (fixed monitoring reload window)

--- modules/custom/fileedit/assets/js/fileedit.js
+++ modules/custom/fileedit/assets/js/fileedit.js
@@ -1,5 +1,6 @@
 jQuery(function ($) {
     $(document).ready(function () {
+        window.fileEditWin = null;
         window.fileEditConfigurations = new Array();
 
         function runConfiguration(downloadUrl, filename, uploadUrl, uploadFieldName) {
@@ -7,16 +8,15 @@ jQuery(function ($) {
                 downloadUrl:downloadUrl,
                 filename:filename,
                 uploadUrl:uploadUrl,
-                uploadFieldName:uploadFieldName,
-
+                uploadFieldName:uploadFieldName
             };
 
             window.fileEditConfigurations.push(configuration);
 
             if (!window.fileEditWin.isAlive || !window.fileEditWin.isAlive()) {
                 if (window.fileEditWin.showMessage) {
-                    window.fileEditWin.showMessage('Please make sure that you have activated and trusted the applet to Edit the file ' + filename + '. Your request was added to the queue and will be handled as soon as applet is loaded.');
-//                    alert('Please make sure that you have activated and trusted the applet to Edit the file ' + filename + '. Your request was added to the queue and will be handled as soon as applet is loaded.');
+                    window.fileEditWin.showMessage('Please make sure that you have activated and trusted the applet to Edit the file "' + filename + '". Your request was added to the queue and will be handled as soon as applet is loaded.');
+//                    alert('Please make sure that you have activated and trusted the applet to Edit the file "' + filename + '". Your request was added to the queue and will be handled as soon as applet is loaded.');
                 }
             }
         }
@@ -34,15 +34,28 @@ jQuery(function ($) {
             var downloadUrl = Drupal.settings.fileedit.downloadUrl.replace("%25", fid).replace('%25', Drupal.settings.fileedit.sid);
             var uploadUrl = Drupal.settings.fileedit.uploadUrl.replace("%25", fid).replace('%25', Drupal.settings.fileedit.sid);
 
-            if (window.fileEditWin == undefined || window.fileEditWin.closed) {
+            var reopen = !window.fileEditWin || window.fileEditWin.closed;
+            if (reopen) {
                 // http://www.pageresource.com/jscript/jwinopen.htm
-                window.fileEditWin = window.open(Drupal.settings.fileedit.appletUrl, 'FileEdit', "status=no,resizable=no,width=415,height=315");
-                window.fileEditWin.onload = function (e) {
-                    setTimeout(function () {
-                        runConfiguration(downloadUrl, filename, uploadUrl, Drupal.settings.fileedit.uploadFieldName);
-                    }, 1000);
+                window.fileEditWin = window.open('', 'FileEdit', "status=no,resizable=no,width=415,height=315");
+                if (window.fileEditWin) {
+                    if (window.fileEditWin.location == 'about:blank') {
+                        window.fileEditWin.location = Drupal.settings.fileedit.appletUrl;
+
+                        window.fileEditWin.onload = function (e) {
+                            setTimeout(function () {
+                                runConfiguration(downloadUrl, filename, uploadUrl, Drupal.settings.fileedit.uploadFieldName);
+                            }, 1500);
+                        }
+                    } else {
+                        reopen = false;
+                    }
+                } else {
+                    alert('Unable to Edit "' + filename + '". It looks like the monitoring window is blocked by popup blocker.');
                 }
-            } else {
+            }
+
+            if (window.fileEditWin && !reopen) {
                 window.fileEditWin.focus();
                 runConfiguration(downloadUrl, filename, uploadUrl, Drupal.settings.fileedit.uploadFieldName);
             }
