fa400f7a90c0716b8d6595ce80ba9937a877cb76
Thiemo Müller <t.mueller@morpol.de>
2013-02-12 14:43:06 +0100

    Edited template to show commander console independent of shortcut_keys

--- modules/custom/commander/templates/commander-execute-block.tpl.php
+++ modules/custom/commander/templates/commander-execute-block.tpl.php
@@ -1,6 +1,6 @@
 <div id="commander-execute-block">
   <?php if( $allow_fullscreen ): ?>
-    <a href="#" onclick="shortcut_keys.execute( shortcut_keys.shortcuts.toggle_commander_console ); return false;"><?php print t( 'Show fullscreen' ); ?></a>
+    <a href="#" onclick="commander.toggle_console(); return false;"><?php print t( 'Show fullscreen' ); ?></a>
   <?php endif; ?>
   <div id="commander-execute-block-log">
     <?php foreach( $messages as $entry ) print theme( 'commander_log_entry', array('entry'=>$entry) ); ?>
