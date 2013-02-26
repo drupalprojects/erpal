e3bfde2308cc0554c8a41efb1ea52e1c3d4ac446
Manuel Pistner <pistner@brightsolutions.de>
2013-02-26 11:35:28 +0100

    Removed modules that have now been added as separate project on drupal.org

--- modules/custom/erpal_asterisk/modules/erpal_asterisk_notify/templates/erpal_asterisk_unknown_number.tpl.php
+++
@@ -1,8 +0,0 @@
-<div id="asterisk-notification"; class="erpal-asterisk-notify">  
-  <h1><?php print t('Incoming Call'); ?></h1> 
-  <?php print $output; ?>
-  <div class="asterisk-status-buttons">
-    <a id="asterisk-available"; class="button"><?php print t('Available'); ?></a>
-    <a id="asterisk-not-available"; class="button"><?php print t('Not available'); ?></a>
-  </div>
-</div> 
