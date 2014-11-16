f65886a61dff2f24c76204cbebd2cb5a3c1c31cd
Manuel Pistner <pistner@brightsolutions.de>
2014-11-16 15:43:34 +0100

    Fixed search pane.

--- modules/profile/erpal_search_pane/js/erpal_search_pane.js
+++ modules/profile/erpal_search_pane/js/erpal_search_pane.js
@@ -3,10 +3,10 @@ jQuery(document).ready(function($) {
   var pathname = window.location.pathname;
 
   if(pathname == "/erpal-search") {
-    var search_string = $('.view-id-erpal_search .hidden-search-string').attr('value');
+    var search_string = $('.hidden-search-string').attr('value');
     if(search_string != '') {
-      $('.view-id-erpal_search input#edit-search-api-views-fulltext').attr('value',search_string)
-      $('.view-id-erpal_search .views-submit-button input').trigger('click');
+      $('input#edit-search-api-views-fulltext').attr('value',search_string)
+      $('.views-submit-button input').trigger('click');
     }
   }
 });
